a37a0c91cabbbf2416702d0efd71f27cd2850665
albertomonge <alberto.monge@polito.it>
2020-06-27 21:40:00 +0200

    Rimossa password db

--- src/main/java/it/polito/tdp/PremierLeague/db/DBConnect.java
+++ src/main/java/it/polito/tdp/PremierLeague/db/DBConnect.java
@@ -17,7 +17,7 @@ public class DBConnect {
 			HikariConfig config = new HikariConfig();
 			config.setJdbcUrl(jdbcURL);
 			config.setUsername("root");
-			config.setPassword("rootroot");
+			config.setPassword("");
 			
 			// configurazione MySQL
 			config.addDataSourceProperty("cachePrepStmts", "true");
